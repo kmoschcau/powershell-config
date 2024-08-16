a39a5607e19c11691a18b2e6d629b15ab6ddc0d9
Kai Moschcau <kai.moschcau@blecon.de>
2024-08-16 10:03:47 +0200

    feat: add winget completion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -912,6 +912,20 @@ param(
   }
 }
 
+# }}}
+
+# winget {{{
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
 # }}}}}}
 
 # Chocolatey {{{
