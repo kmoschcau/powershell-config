65ef54d256e556278f45e7785ec4afc63af37705
Kai Moschcau <kai.moschcau@blecon.de>
2024-09-23 09:13:23 +0200

    feat: add yazi and zoxide integration

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -941,3 +941,23 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 # }}}
+
+# Yazi {{{
+
+function y {
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file=$tmp
+  $cwd = Get-Content -Path $tmp
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+    Set-Location -LiteralPath $cwd
+  }
+  Remove-Item -Path $tmp
+}
+
+# }}}
+
+# Zoxide {{{
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# }}}
